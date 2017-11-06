d3a1b8da1e9e64dc9c7cc04a8dfc571bf98ee76c
Fabian PETRAS <fpetras@e1r12p11.42.fr>
2017-11-06 18:47:03 +0100

    Creation of ft_display_file.c and Makefile for Exercise 27

---
+++ ex27/ft_display_file.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/06 18:09:00 by fpetras           #+#    #+#             */
+/*   Updated: 2017/11/06 18:45:55 by fpetras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int		main(int ac, char **av)
+{
+	int fd;
+	char buf;
+
+	if (ac == 1)
+	{
+		write(1, "File name missing.\n", 19);
+		return (1);
+	}
+	if (ac > 2)
+	{
+		write(1, "Too many arguments.\n", 20);
+		return (1);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while (read(fd, &buf, 1) != 0)
+		write(1, &buf, 1);
+	close(fd);
+	return (0);
+}
